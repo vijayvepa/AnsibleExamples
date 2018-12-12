78ea0caf9a5f0e0ed42ac206ab2e83a9fcf5003e
Vijay Vepakomma <vijay.vepakomma@philips.com>
2018-12-11 23:21:05 -0500

    added custom settings

---
+++ 02.Django-RestApi/10.installmysql.sh
@@ -0,0 +1,1 @@
+brew install homebrew/versions/mysql56
---
+++ 02.Django-RestApi/src/todobackend/settings/test.py
@@ -0,0 +1,13 @@
+from todobackend.settings.base import *
+import os
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('MYSQL_DATABASE', 'todobackend'),
+        'USER': os.environ.get('MYSQL_USER', 'todo'),
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD', 'password'),
+        'HOST': os.environ.get('MYSQL_HOST', 'localhost'),
+        'PORT': os.environ.get('MYSQL_PORT', '3306')
+    }
+}
