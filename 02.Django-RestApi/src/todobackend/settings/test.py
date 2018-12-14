19215f6a58940691bc0486860b3690e31903761c
Vijay Vepakomma <vijay.vepakomma@philips.com>
2018-12-14 11:28:11 -0500

    setup test reporting

--- 02.Django-RestApi/src/todobackend/settings/test.py
+++ 02.Django-RestApi/src/todobackend/settings/test.py
@@ -1,6 +1,24 @@
 from todobackend.settings.base import *
 import os
 
+# Installed Apps
+INSTALLED_APPS += ('django_nose', )
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+TEST_OUTPUT_DIR = os.environ.get('TEST_OUTPUT_DIR', '.')
+
+NOSE_ARGS = [
+    '--verbosity=2',
+    '--nologcapture',
+    '--with-coverage',
+    '--cover-package=todo',
+    '--with-spec', #spec-style-tests
+    '--spec-color',
+    '--with-xunit',
+    '--xunit-file=%s/unittests.xml' % TEST_OUTPUT_DIR,
+    '--cover-xml',
+    '--cover-xml-file=%s/coverage.xml' % TEST_OUTPUT_DIR
+]
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
